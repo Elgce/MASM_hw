ef9a2de4ad84f7d1f496218c0d04c5aa686e4cf3
Elgce <bqw20@mails.tsinghua.edu.cn>
2022-10-16 21:24:13 +0800

    完成了服务器新增客户端登录的用户功能

--- Client/WinApp_v2 - 副本.asm
+++ Client/WinApp_v2 - 副本.asm
@@ -8,11 +8,9 @@ include user32.inc
 include kernel32.inc
 includelib user32.lib
 includelib kernel32.lib
+include Message.inc
 include		wsock32.inc
 includelib	wsock32.lib
-includelib      msvcrt.lib
-printf          PROTO C :ptr sbyte, :VARARG
-include Message.inc
 
 .data
 hInstance	DWORD	?
@@ -28,8 +26,6 @@ empty_ms	DWORD	256 dup(?)
 szServer	BYTE	16 dup(?)
 szUserName	db		12 dup(?)
 hSocket		DWORD	?
-dwSendBufSize	dd	?
-dwRecvBufSize	dd	?
 
 szSendMsg	MSG_STRUCT	10 dup (<>)
 
@@ -81,45 +77,12 @@ _SendData	proc	_lpData,_dwSize
 	mov ecx, _dwSize
 	.if esi & ecx
 		push	ecx
-		mov	edi,offset szSendMsg
-		add	edi,dwSendBufSize
-		cld
-		rep	movsb
-		pop	ecx
-		add	dwSendBufSize,ecx
-	.endif
-	;发送到缓冲区
-	mov esi,offset szSendMsg
-	mov ebx,dwSendBufSize
-	invoke send,hSocket,esi,ebx,0
-	ret
-_SendData	endp
-
-;连接服务器
-_Connect	proc
-		local	@stSin:sockaddr_in
-		local	@szBuffer[512]:byte
-		pushad
-		
-		;创建Socket
-		invoke	RtlZeroMemory,addr @stSin, sizeof @stSin
-		invoke	inet_addr,addr szServer
-		mov	@stSin.sin_addr,eax
-		mov	@stSin.sin_family,AF_INET
-		invoke	htons,TCP_PORT
-		mov	@stSin.sin_port,ax
-		invoke	socket,AF_INET,SOCK_STREAM,0
-		mov	hSocket,eax
-		;连接服务器，这里没有设置成非阻塞模式
-		invoke	connect,hSocket,addr @stSin,sizeof @stSin
-		ret
-_Connect	endp
 
 
 ;主程序函数
 MainProc proc,
 	hWnd:DWORD, uMsg:DWORD, wParam:DWORD, lParam:DWORD
-	local	@stWsa:WSADATA,@stMsg:MSG_STRUCT
+	local	@stWsa:WSADATA
 	mov eax,uMsg
 	.if eax == WM_CLOSE
 		invoke EndDialog,hWnd,0
@@ -135,51 +98,30 @@ MainProc proc,
 		; TODO: 完成发送和显示的逻辑
 			;通过massage储存信息
 			invoke GetDlgItemText,hWnd,IDC_EDIT,addr massage,sizeof massage
-			.if massage
-				invoke SetDlgItemText,hWnd,IDC_EDIT,NULL
-				;串联出最终需要的消息内容到user_ms
-				cld
-				mov ecx, LENGTHOF empty_ms
-				mov esi, OFFSET empty_ms
-				mov edi, OFFSET user_ms
-				rep movsd
-				; TODO： 接口获取时间字符串，连接到user_ms
-				invoke lstrcat, addr user_ms, addr @line
-				invoke lstrcat, addr user_ms, addr user
-				invoke lstrcat, addr user_ms, addr @line ; 拼接用户名字
-				invoke SendDlgItemMessage,hWnd,IDC_EDIT_MASSAGE, LB_ADDSTRING, 0,addr user_ms
-				invoke SendDlgItemMessage,hWnd,IDC_EDIT_MASSAGE, LB_ADDSTRING, 0,addr massage
-				invoke lstrcpy,addr @stMsg.MsgUp.szContent,addr massage
-				invoke lstrcpy,addr @stMsg.MsgUp.szSender,addr szUserName
-				invoke lstrlen,addr @stMsg.MsgUp.szContent
-				inc eax
-				mov @stMsg.MsgUp.dwLength,eax
-				add eax,sizeof MSG_HEAD+MSG_UP.szContent+MSG_UP.szSender
-				mov	@stMsg.MsgHead.dwLength, eax
-				invoke _SendData,addr @stMsg,@stMsg.MsgHead.dwLength
-			.endif
+			invoke SetDlgItemText,hWnd,IDC_EDIT,NULL
+			;串联出最终需要的消息内容到user_ms
+			cld
+			mov ecx, LENGTHOF empty_ms
+			mov esi, OFFSET empty_ms
+			mov edi, OFFSET user_ms
+			rep movsd
+			; TODO： 接口获取时间字符串，连接到user_ms
+			invoke lstrcat, addr user_ms, addr @line
+			invoke lstrcat, addr user_ms, addr user
+			invoke lstrcat, addr user_ms, addr @line ; 拼接用户名字
+			invoke SendDlgItemMessage,hWnd,IDC_EDIT_MASSAGE, LB_ADDSTRING, 0,addr user_ms
+			invoke SendDlgItemMessage,hWnd,IDC_EDIT_MASSAGE, LB_ADDSTRING, 0,addr massage
 		.elseif ax == IDC_LIST_FRIENDS
 		; TODO: 根据选中的用户显示聊天内容
 			invoke SendDlgItemMessage, hWnd, IDC_LIST_FRIENDS, LB_GETCURSEL, 0, 0
 			invoke SendDlgItemMessage, hWnd, IDC_LIST_FRIENDS, LB_GETTEXT, ax, addr user
 			invoke SetDlgItemText,hWnd,IDC_STATIC_FRIENDNAME,addr user
 		.elseif	ax == IDC_LOGIN
-			;invoke	GetDlgItemText,hWnd,IDC_EDIT_IP,addr szServer,sizeof szServer
-			;push	ecx
-			;invoke	CreateThread,NULL,0,offset _WorkThread,0,NULL,esp
-			;pop	ecx
-			;invoke	CloseHandle,eax
 			invoke	GetDlgItemText,hWnd,IDC_EDIT_IP,addr szServer,sizeof szServer
-			invoke	GetDlgItemText,hWnd,IDC_EDIT_NAME,addr szUserName, sizeof szUserName
-			invoke _Connect
-			invoke lstrcpy,addr @stMsg.MsgUp.szContent,addr massage
-			invoke lstrcpy,addr @stMsg.MsgUp.szSender,addr szUserName
-			invoke lstrlen,addr @stMsg.MsgUp.szContent
-			inc eax
-			mov @stMsg.MsgUp.dwLength,eax
-			add eax,sizeof MSG_HEAD+MSG_UP.szContent+MSG_UP.szSender
-			mov	@stMsg.MsgHead.dwLength, eax
-			invoke _SendData,addr @stMsg,@stMsg.MsgHead.dwLength
+			push	ecx
+			invoke	CreateThread,NULL,0,offset _WorkThread,0,NULL,esp
+			pop	ecx
+			invoke	CloseHandle,eax
 		.endif
 	.else
 		mov eax,FALSE
--- Server/WinApp_v2.asm
+++ Server/WinApp_v2.asm
@@ -1,3 +1,4 @@
+;server
 .386
 .model flat,stdcall
 option casemap:none
@@ -9,12 +10,28 @@ include		kernel32.inc
 includelib	kernel32.lib
 include		wsock32.inc
 includelib	wsock32.lib
+includelib      msvcrt.lib
+printf          PROTO C :ptr sbyte, :VARARG
+;include		_SocketRoute.asm
+;include		_MsgQueue.asm
+;include		Message.inc
 ICO_MAIN	equ	1000
 DLG_MAIN	equ	2000
 IDC_COUNT	equ	2001
 TCP_PORT	equ	9999
 
+
+
+;客户端会话消息
+SESSION	struct
+	szUserName	db 12 dup(?) ;用户名
+	dwMessageId	dd	?	;已经下发的消息编号
+	dwLastTime	dd	?	;链路最近一次活动时间
+SESSION	ends
+
+
 .data
+dwSequence	dd	1
 hInstance	dd	?
 hWinMain	dd	?
 hListenSocket	dd	?
@@ -28,7 +45,7 @@ user3       BYTE	'lzy',0
 user        BYTE	8 dup(?)
 users		DWORD	100 dup(?)
 
-.const
+.const	
 IDD_MAIN                  =          101
 IDC_EDIT                  =          1001
 IDC_LIST_CLIENTS          =          1002
@@ -40,20 +57,132 @@ IDC_STATIC_NUM            =          1007
 szErrBind	db	'无法绑定到TCP端口9999，请检查是否有其它程序在使用!',0
 
 .code
+include	Message.inc
+		assume esi:ptr MSG_STRUCT,edi:ptr SESSION
+
+_WaitData	proc	_hSocket,_dwTime
+		local	@stFdSet:fd_set,@stTimeval:timeval
+
+		mov	@stFdSet.fd_count,1
+		push	_hSocket
+		pop	@stFdSet.fd_array
+		push	_dwTime
+		pop	@stTimeval.tv_usec
+		mov	@stTimeval.tv_sec,0
+		invoke	select,0,addr @stFdSet,NULL,NULL,addr @stTimeval
+		ret
+
+_WaitData	endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+; 接收规定字节的数据，如果缓冲区中的数据不够则等待
+; 返回：eax = TRUE，连接中断或发生错误
+;	eax = FALSE，成功
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_RecvData	proc	_hSocket,_lpData,_dwSize
+		local	@dwStartTime
+
+		mov	esi,_lpData
+		mov	ebx,_dwSize
+		invoke	GetTickCount
+		mov	@dwStartTime,eax
+;********************************************************************
+		@@:
+		invoke	GetTickCount			;查看是否超时
+		sub	eax,@dwStartTime
+		cmp	eax,10 * 1000
+		jge	_Err
+;********************************************************************
+		invoke	_WaitData,_hSocket,100*1000	;等待数据100ms
+		cmp	eax,SOCKET_ERROR
+		jz	_Err
+		or	eax,eax
+		jz	@B
+		invoke	recv,_hSocket,esi,ebx,0
+		.if	(eax == SOCKET_ERROR) || ! eax
+_Err:
+			xor	eax,eax
+			inc	eax
+			ret
+		.endif
+		.if	eax <	ebx
+			add	esi,eax
+			sub	ebx,eax
+			jmp	@B
+		.endif
+		xor	eax,eax
+		ret
+
+_RecvData	endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+; 接收一个符合规范的数据包
+; 返回：eax = TRUE （失败）
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+_RecvPacket	proc	_hSocket,_lpBuffer,_dwSize
+		local	@dwReturn
+
+		pushad
+		mov	@dwReturn,TRUE
+		mov	esi,_lpBuffer
+		assume	esi:ptr MSG_STRUCT
+;********************************************************************
+; 接收数据包头部并检测数据是否正常
+;********************************************************************
+		invoke	_RecvData,_hSocket,esi,sizeof MSG_HEAD
+		or	eax,eax
+		jnz	_Ret
+		mov	ecx,[esi].MsgHead.dwLength
+		cmp	ecx,sizeof MSG_HEAD
+		jb	_Ret
+		cmp	ecx,_dwSize
+		ja	_Ret
+;********************************************************************
+; 接收余下的数据
+;********************************************************************
+		sub	ecx,sizeof MSG_HEAD
+		add	esi,sizeof MSG_HEAD
+		.if	ecx
+			invoke	_RecvData,_hSocket,esi,ecx
+		.else
+			xor	eax,eax
+		.endif
+		mov	@dwReturn,eax
+_Ret:
+		popad
+		assume	esi:nothing
+		mov	eax,@dwReturn
+		ret
+
+_RecvPacket	endp
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
 ; 通讯服务线程：每个客户端登录的连接将产生一个线程
 _ServiceThread	proc	_hSocket
+		local @szBuffer[512]:byte,@stSession:SESSION
+		pushad
 		inc	dwThreadCounter
 		invoke	SetDlgItemInt,hWinMain,IDC_NUM,dwThreadCounter,FALSE
+		
+		lea	esi,@szBuffer
+		lea edi,@stSession
+		invoke	RtlZeroMemory,edi,sizeof @stSession
+		mov eax,dwSequence
+		mov [edi].dwMessageId,eax
+		invoke	_RecvPacket,_hSocket,esi,sizeof @szBuffer
+		assume	esi:ptr MSG_STRUCT
+		invoke SendDlgItemMessage, hWinMain, IDC_LIST_CLIENTS, LB_ADDSTRING, 0, addr [esi].MsgUp.szSender
+
 		.while	! (dwFlag & F_STOP)
-			;在这里死循环
+		;消息循环
 		.endw
 		invoke	closesocket,_hSocket
 		dec	dwThreadCounter
 		invoke	SetDlgItemInt,hWinMain,IDC_NUM,dwThreadCounter,FALSE
+		popad
 		ret
 
 _ServiceThread	endp
-
+	assume esi:nothing,edi:nothing
 ; 监听线程
 _ListenThread	proc	_lParam
 		local	@stSin:sockaddr_in
